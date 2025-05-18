fa3fd63f7a9ba4d2db10969277f54a90cfa4cd43
Mathias Hamp <hamp.mathias@gmail.com>
2025-05-18 10:04:57 +0200

    MR-00 Initial commit

---
+++ authentication/src/test/java/com/xontext/MainTest.java
@@ -0,0 +1,25 @@
+package com.xontext;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+class MainTest {
+    @Test
+    public void testMainOutput() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        Main.main(new String[]{});
+        System.setOut(originalOut);
+
+        String actual = outContent.toString().replace("\r\n", "\n").trim();
+        String expected = ("Hello and welcome!").trim();
+
+        assertEquals("Messages should be the same", expected, actual);
+    }
+}
