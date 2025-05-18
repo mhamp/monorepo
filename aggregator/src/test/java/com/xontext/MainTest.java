794223c4239c920085d23914884667e613036905
Mathias Hamp <hamp.mathias@gmail.com>
2025-05-19 00:14:47 +0200

    MR-00 Cleaned POM and added more unit tests

---
+++ aggregator/src/test/java/com/xontext/MainTest.java
@@ -0,0 +1,26 @@
+package com.xontext;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintStream;
+
+class MainTest {
+    @Test
+    public void testMainOutput() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        Main.main(new String[]{});
+        System.setOut(originalOut);
+
+        String actual = outContent.toString().replace("\r\n", "\n").trim();
+        String expected = ("Hello and welcome!").trim();
+
+        assertEquals(expected, actual, "Messages should be the same");
+    }
+
+    private void assertEquals(String expected, String actual, String messagesShouldBeTheSame) {
+    }
+}
